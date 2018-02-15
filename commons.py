b1f84cf1b02acbf9001c53918a5b76d19287ece5
Vladimir <vladimir.tuboltsev@gmail.com>
2018-02-15 22:36:31 +0300

    reset was added

--- commons.py
+++ commons.py
@@ -12,18 +12,10 @@ def find_and_click(img):
         pyautogui.click(b_location[0], b_location[1])  # close message
         return True
 
-
 def close_window():
     ''' try to find close button "X" and click it
         :returns
             True - if button was found
             False - if button wasn't found
         '''
-    b_close_location = ocv.locateOnScreen('data/b_x.png')
-
-    if b_close_location:
-        buttonx, buttony = pyautogui.center(b_close_location)
-        pyautogui.click(buttonx, buttony)  # clicks the center of where the button was found
-        return True
-
-    return False
+    return find_and_click('data/b_x.png')
--- vikingclicker.py
+++ vikingclicker.py
@@ -7,7 +7,7 @@ pyautogui.PAUSE = 2
 
 from time import sleep
 
-from commons import close_window
+from commons import close_window, find_and_click
 
 import quests
 import help
@@ -24,8 +24,11 @@ def main(cycles=1):
     hero_flag = True
     while(cycles > 0):
         print('cycles: %s'%cycles)
-        close_window()
 
+        # close window if it's open
+        find_and_click('data/b_x.png')
+
+        #main part
         quests.run()
         if not hero_flag:
             hero_flag = patrol.kill_mobs('2')
@@ -34,6 +37,9 @@ def main(cycles=1):
 
         if cycles%15 == 0:
             hero_flag = False
+
+        # reset application if connection is broke
+        find_and_click('data/reset_b.png')
 
         cycles -= 1
         sleep(360)
