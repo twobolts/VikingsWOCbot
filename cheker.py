28793ad403d8e4ffad334408fc028d84770ca8fa
Vladimir <vladimir.tuboltsev@gmail.com>
2018-02-25 22:12:53 +0300

    fix the realisation

--- cheker.py
+++ cheker.py
@@ -2,7 +2,7 @@ import pyautogui
 from time import sleep
 
 import ocv
-from commons import close_window
+from commons import close_window, move_to_center
 
 def open_klans():
     pyautogui.press('c')
@@ -32,17 +32,26 @@ def get_member_positions():
 
     member_list = ocv.locateAllOnScreen('data/klan_member_arrow.png')
 
+    member_pos = []
+
     for member in member_list:
 
         pyautogui.click(member[0],member[1])
         #pyautogui.click()
 
         gorod_pos = Player.get_player_positions()
-        print(gorod_pos)
+        x = ''.join([str(x) for x in gorod_pos[0]])
+        y = ''.join([str(x) for x in gorod_pos[1]])
+        member_pos.append((x,y))
 
         #pyautogui.moveTo(gorod_pos[0], gorod_pos[1])
 
         close_window()
+
+    while close_window():
+        move_to_center()
+
+    return check_sheld(member_pos)
 
 def check_sheld(list_of_positions):
     from commons import goto, close_window
@@ -53,18 +62,20 @@ def check_sheld(list_of_positions):
         goto(pos[0],pos[1])
 
         gor_pos = ocv.locateCenterOnScreen('data/map_gorod.png')
-        pyautogui.click(gor_pos[0],gor_pos[1]-450)
+        pyautogui.click(gor_pos[0],gor_pos[1]-400)
+        sleep(2)
 
         #TODO: check sheld
         is_sheld = ocv.locateCenterOnScreen('data/map_sheld.png')
 
         if not is_sheld:
-            print(pos)
+            res.append(pos)
 
         close_window()
+    return res
 
 
 if __name__ == "__main__":
     from commons import test
 
-    test(get_member_positions)
+    print (test(get_member_positions))
